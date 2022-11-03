1003c02d1b79001d21e46ea626c8890852894216
Jacquiiii <jacqui.koroll@telus.com>
2022-11-03 12:51:56 -0700

    updating code page to include build a triangle quiz code block

--- udacity-functions.js
+++ udacity-functions.js
@@ -63,3 +63,22 @@ function test() {
 test();
 */
 
+/* quiz #3 make a triangle
+function makeLine(length) {
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+    line += "* "
+  }
+  return line + "\n";
+}
+function buildTriangle(length) {
+  var triangle = "";
+  var lineNumber = 1
+  for (lineNumber = 1; lineNumber <= length; lineNumber++) {
+    triangle = triangle + makeLine(lineNumber);
+  }
+  return triangle;
+}
+console.log(buildTriangle(10));
+*/
+
