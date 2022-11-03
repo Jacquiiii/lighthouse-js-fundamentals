d15a9b6f4b124b793b0b0708a5e9f156c300ffa8
Jacquiiii <jacqui.koroll@telus.com>
2022-11-03 11:25:44 -0700

    adding more code to the udacity functions document

--- udacity-functions.js
+++ udacity-functions.js
@@ -1,4 +1,4 @@
-/* first exercise:
+/* 
 function findAverage(x, y) {
   var answer = (x + y) / 2;
   return answer;
@@ -7,15 +7,16 @@ var avg = findAverage(5, 9);
 console.log(avg);
 */
 
-/* quiz #1:
+/* 
+quiz #1:
 function laugh() {
   return "hahahahahahahahahaha!"
 }
 console.log(laugh());
 */
 
-// quiz #2:
-
+/* 
+quiz #2:
 var sound = "";
 function laugh(num) {
   for (var x = 0; x < num; x++) {
@@ -25,3 +26,40 @@ function laugh(num) {
   return sound;
 }
 console.log(laugh(5));
+*/
+
+/*
+function isThisWorking(input) {
+  console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+  return "Returning: I am returning this string!";
+}
+console.log(isThisWorking(3));
+*/
+
+/*
+function sleep() {
+  console.log("I'm sleepy!");
+  return "zzz";
+  return "snore";
+}
+sleep();
+*/
+
+/*
+function square(x) {
+  return x * x;
+}
+function subtractFour(x) {
+  return square(x) - 4;
+}
+console.log(subtractFour(5));
+*/
+
+/*
+function test() {
+  return 1;
+  return 2;
+}
+test();
+*/
+
