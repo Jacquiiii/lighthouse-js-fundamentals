6da6de8d073dc15395fbfc5b29778538384083ac
Jacquiiii <jacqui.koroll@telus.com>
2022-11-03 14:12:56 -0700

    final audacity functions work

--- udacity-functions.js
+++ udacity-functions.js
@@ -82,3 +82,52 @@ function buildTriangle(length) {
 console.log(buildTriangle(10));
 */
 
+/* 
+var catSays = function(max) {
+  var catMessage = "";
+  for (var i = 0; i < max; i++) {
+    catMessage += "meow ";
+  }
+  return catMessage;
+};
+function helloCat(callbackFunc) {
+  return "Hello " + callbackFunc(3);
+}
+helloCat(catSays);
+*/
+
+/* 
+quiz #4:
+var laugh = function (num) {
+  var sound = ""
+  for (var x = 0; x < num; x++) {
+    sound = sound + "ha";
+  }
+  sound = sound +"!";
+  return sound;
+}
+console.log(laugh(10));
+*/
+
+/* 
+quiz #5:
+var cry = function booHoo() {
+  return "boohoo!";
+}
+console.log(cry());
+*/
+
+/* quiz #6: 
+function emotions(myString, myFunc) {
+  console.log("I am " + myString + ", " + myFunc(2));
+}
+emotions ("happy", function(num) {
+  var sound = ""; 
+    for (var i = 0 ; i < num ; i++) {
+        sound = sound + "ha" ;  
+    }
+    sound = sound +"!"; 
+    return sound; 
+});
+*/
+
