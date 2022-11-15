8a10951b15401e140a721263b570fed33cdd25af
Jacquiiii <jacqui.koroll@telus.com>
2022-11-15 12:30:13 -0800

    added comment with alt method for loopy lighthouse challenge

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -9,3 +9,20 @@ for (let num = 100; num <= 200; num++) {
     console.log(num);
   }
 }
+
+
+/* Alternative:
+
+for (const num of nums) {
+  let output = "";
+
+  if (num % 3 === 0) {
+    output += "Loopy";
+  }
+  if (num % 4 === 0) {
+    output += "Lighthouse";
+  }
+  console.log(output === "" ? num : output);
+}
+
+*/
